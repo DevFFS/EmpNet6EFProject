4b1fe52d90a712bfecdd0b2a931f77c3d715d6e8
Ayaz Hassan <ayazhassansiddiq@gmail.com>
2023-10-12 20:45:28 +0530

    added project

---
+++ WebApiCoreWithEF/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiCoreWithEF.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeeId { get; set; }
+
+        public string EmployeeName { get; set; } = null!;
+
+        public string DateOfBirth { get; set; } = null!;
+
+        public string Gender { get; set; } = null!;
+
+        public string CurrentAddress { get; set; } = null!;
+
+        public string PermanentAddress { get; set; } = null!;
+
+        public string City { get; set; } = null!;
+
+        public string Nationality { get; set; } = null!;
+
+        public int PINCode { get; set; }
+    }
+}
